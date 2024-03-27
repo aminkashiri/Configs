d1143795c608139d8c7f3546cddb0b07b2de6ddf
Amin Kashiri <kashiri.amin@gmail.com>
2024-03-27 08:47:07 -0400

    Add init_venv command

--- .scripts/venv.sh
+++ .scripts/venv.sh
@@ -1,3 +1,16 @@
 function venv() { 
     source .venv/bin/activate
 } 
+
+function init_venv() {
+    if [ ! -d ".venv" ]; then
+        echo "No venv. Creating one"
+        python -m venv .venv
+        echo "Activating venv"
+        venv
+        if [ -f "requirements.txt" ]; then
+            echo "Found requirements.txt. Installing packages in venv..."
+            pip install -r requirements.txt
+        fi
+    fi
+}
